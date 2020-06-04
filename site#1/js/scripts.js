2fd36a9f5d0b26490a547c8a7682571d93604028
prazdnikovamargarita <prazdnikovamargarita@gmail.com>
2020-06-04 13:32:03 +0300

    first template of first page

---
+++ site#1/js/scripts.js
@@ -0,0 +1,7 @@
+document.createElement("header");
+var e = ("article,aside,figcaption,figure,footer,header,hgroup,nav,section,time").split(',');
+  for (var i = 0; i < e.length; i++) {
+    document.createElement(e[i]);
+  }
+
+
